710a63d2698b0e122a9c944fce06df772419d5d7
MrYamiTsu <mryamitsu@hotmail.com>
2025-07-17 20:43:21 -0400

    Add Jack Mutator to the main menu

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+-- Loading Files --
 SMODS.load_file("items/booster.lua")()
 SMODS.load_file("items/enhancement.lua")()
 SMODS.load_file("items/food.lua")()
@@ -8,6 +9,7 @@ if next(SMODS.find_mod("Cryptid")) then
 	SMODS.load_file("items/CrossMod/cryptid.lua")()
 end
 
+-- Atlas --
 SMODS.Atlas{
     key = "modicon", 
     path = "modicon.png", 
@@ -85,3 +87,41 @@ SMODS.Rarity{
     },
 	badge_colour = HEX("dbd809"),
 }
+
+-- Main Menu --
+
+SMODS.Joker{
+	key = "mainMenuJoker",
+	atlas = "Jokers",
+	rarity = "giga_megaLegendary",
+	cost = 0,
+	unlocked = true,
+	discovered = true,
+	no_collection = true,
+	pos = {x = 4, y = 1},
+	in_pool = function(self)
+		return false
+	end,
+}
+
+local oldfunc = Game.main_menu 
+Game.main_menu = function(change_context)
+	local idk = oldfunc(change_context)
+	local newcard = Card(
+		G.title_top.T.x,
+		G.title_top.T.y,
+		G.CARD_W,
+		G.CARD_H,
+		G.P_CARDS.empty,
+		G.P_CENTERS.j_giga_mainMenuJoker,
+		{ bypass_discovery_center = true }
+	)
+    G.title_top.T.w = G.title_top.T.w * 1.3
+	G.title_top.T.x = G.title_top.T.x - 0.8
+	G.title_top:emplace(newcard)
+    newcard.T.w = newcard.T.w * 1.1 * 1.25
+	newcard.T.h = newcard.T.h * 1.1 * 1.25
+	newcard.no_ui = true
+	newcard.states.visible = true
+end
+-- From Revo'sVault, that was from Cryptid. (So thx Cryptid)
