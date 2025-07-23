ae9dd1f9bb95f899b89e5f8dcb96ff578fe3ff16
MrYamiTsu <mryamitsu@hotmail.com>
2025-07-23 18:07:58 -0400

    Update Food and Joker texture

--- items/food.lua
+++ items/food.lua
@@ -282,7 +282,7 @@ SMODS.Consumable{ --Steak
     set = 'Giga_Food',
     atlas = 'Foods',
     pos = {x = 0, y = 0},
-    soul_pos = {x = 0, y = 1},
+    soul_pos = {x = 2, y = 2},
     rarity = 1,
     cost = 2,
     config = { extra = {
