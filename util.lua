32dfe7d8bbf0e7a0005fbe578feb879dcc97787f
MrYamiTsu <mryamitsu@hotmail.com>
2025-07-26 11:32:24 -0400

    Add flip animation to upgrade function

--- util.lua
+++ util.lua
@@ -1,32 +1,66 @@
 function upgrade_enhencement(selected_card)
-    if SMODS.has_enhancement(selected_card, 'm_giga_soil') then
-        selected_card:set_ability(G.P_CENTERS["m_giga_richSoil"])
-    elseif SMODS.has_enhancement(selected_card, 'm_giga_richSoil') then
-        selected_card:set_ability(G.P_CENTERS["m_giga_fossilSoil"])
-    elseif SMODS.has_enhancement(selected_card, 'm_bonus') then
-        selected_card:set_ability(G.P_CENTERS["m_giga_bigBonus"])
-    elseif SMODS.has_enhancement(selected_card, 'm_stone') then
-        selected_card:set_ability(G.P_CENTERS["m_giga_polishStone"])
-    elseif SMODS.has_enhancement(selected_card, 'm_mult') then
-        selected_card:set_ability(G.P_CENTERS["m_giga_multPlus"])
-    elseif SMODS.has_enhancement(selected_card, 'm_lucky') then
-        selected_card:set_ability(G.P_CENTERS["m_giga_luckiest"])
-    elseif SMODS.has_enhancement(selected_card, 'm_gold') then
-        selected_card:set_ability(G.P_CENTERS["m_giga_perfectGold"])
-    --[[elseif SMODS.has_enhancement(selected_card, 'm_glass') then
-        selected_card:set_ability(G.P_CENTERS["m_giga_reinforcedGlass"])
-    elseif SMODS.has_enhancement(selected_card, 'm_steel') then
-        selected_card:set_ability(G.P_CENTERS["m_giga_titanium"])]]
-    end
     G.E_MANAGER:add_event(Event({
-		trigger = "after",
-		delay = 0.2,
-		func = function()
-			G.hand:unhighlight_all()
-			return true
-		end,
-	}))
-	delay(0.5)
+        trigger = 'after',
+        delay = 0.4,
+        func = function()
+            selected_card:juice_up(0.3, 0.5)
+            return true
+        end
+    }))
+    G.E_MANAGER:add_event(Event({
+        trigger = 'after',
+        delay = 0.15,
+        func = function()
+            selected_card:flip()
+            selected_card:juice_up(0.3, 0.3)
+            return true
+        end
+    }))
+    delay(0.2)
+    G.E_MANAGER:add_event(Event({
+        trigger = 'after',
+        delay = 0.1,
+        func = function()
+            if SMODS.has_enhancement(selected_card, 'm_giga_soil') then
+                selected_card:set_ability(G.P_CENTERS["m_giga_richSoil"])
+            elseif SMODS.has_enhancement(selected_card, 'm_giga_richSoil') then
+                selected_card:set_ability(G.P_CENTERS["m_giga_fossilSoil"])
+            elseif SMODS.has_enhancement(selected_card, 'm_bonus') then
+                selected_card:set_ability(G.P_CENTERS["m_giga_bigBonus"])
+            elseif SMODS.has_enhancement(selected_card, 'm_stone') then
+                selected_card:set_ability(G.P_CENTERS["m_giga_polishStone"])
+            elseif SMODS.has_enhancement(selected_card, 'm_mult') then
+                selected_card:set_ability(G.P_CENTERS["m_giga_multPlus"])
+            elseif SMODS.has_enhancement(selected_card, 'm_lucky') then
+                selected_card:set_ability(G.P_CENTERS["m_giga_luckiest"])
+            elseif SMODS.has_enhancement(selected_card, 'm_gold') then
+                selected_card:set_ability(G.P_CENTERS["m_giga_perfectGold"])
+            --[[elseif SMODS.has_enhancement(selected_card, 'm_glass') then
+                selected_card:set_ability(G.P_CENTERS["m_giga_reinforcedGlass"])
+            elseif SMODS.has_enhancement(selected_card, 'm_steel') then
+                selected_card:set_ability(G.P_CENTERS["m_giga_titanium"])]]
+            end
+            return true
+        end
+    }))
+    G.E_MANAGER:add_event(Event({
+        trigger = 'after',
+        delay = 0.15,
+        func = function()
+            selected_card:flip()
+            selected_card:juice_up(0.3, 0.3)
+            return true
+        end
+    }))
+    G.E_MANAGER:add_event(Event({
+        trigger = 'after',
+        delay = 0.2,
+        func = function()
+            G.hand:unhighlight_all()
+            return true
+        end
+    }))
+    delay(0.5)
 end
 
 function _create(card,type,place,negative,negative_condition)
